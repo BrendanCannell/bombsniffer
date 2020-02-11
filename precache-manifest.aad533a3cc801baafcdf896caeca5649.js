ad52ce257122426055d8e6c5ba86b0aeeb6bfa11
Brendan Cannell <brendan@cannells.com>
2020-02-11 17:05:47 -0500

    Updates

---
+++ precache-manifest.aad533a3cc801baafcdf896caeca5649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2332bc59a5e476ca12a16636b60e9f8a",
+    "url": "/bombsniffer/index.html"
+  },
+  {
+    "revision": "46556189a886659909cb",
+    "url": "/bombsniffer/static/css/main.8fa39f72.chunk.css"
+  },
+  {
+    "revision": "42277b89432e7615cd1e",
+    "url": "/bombsniffer/static/js/2.d598dd66.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bombsniffer/static/js/2.d598dd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46556189a886659909cb",
+    "url": "/bombsniffer/static/js/main.ccc32c3f.chunk.js"
+  },
+  {
+    "revision": "ce8d9f273373eadc7ad7",
+    "url": "/bombsniffer/static/js/runtime-main.ffa1bd28.js"
+  }
+]);
